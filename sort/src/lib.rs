d88d87b098c29c3589a654d2a430fd1769faec88
t.hcc@foxmail.com <t.hcc@foxmail.com>
2019-05-22 17:33:16 +0800

    add test quick sort

--- sort/src/lib.rs
+++ sort/src/lib.rs
@@ -1,6 +1,7 @@
 use std::cmp::PartialOrd;
 
 mod bubble_sort;
+mod quick_sort;
 
 fn swap<T>(list: &mut [T], l_index: usize, r_index: usize)
 where
@@ -16,10 +17,17 @@ mod tests {
     use super::*;
 
     #[test]
-    fn sort_bubble_sort() {
+    fn test_bubble_sort() {
         let mut list = [8, 100, 50, 22, 15, 6, 1, 1000, 999, 0];
         bubble_sort::sort(&mut list[..]);
         //        list[0] = 2345;
+        println!("{:?}", list);
+    }
+
+    #[test]
+    fn test_quick_sort() {
+        let mut list = [6, 1, 2, 7, 9, 3, 4, 5, 10, 8];
+        quick_sort::sort(&mut list[..]);
         println!("{:?}", list);
     }
 }
---
+++ sort/src/quick_sort.rs
@@ -0,0 +1,4 @@
+
+pub fn sort(list: &mut [i32]) {
+
+}
