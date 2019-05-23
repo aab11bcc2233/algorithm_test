db9f2d74a79fcebefca6a4c94afd48fc8bd440bb
t.hcc@foxmail.com <t.hcc@foxmail.com>
2019-05-23 11:34:09 +0800

    quick sort

--- sort/src/lib.rs
+++ sort/src/lib.rs
@@ -19,15 +19,17 @@ mod tests {
     #[test]
     fn test_bubble_sort() {
         let mut list = [8, 100, 50, 22, 15, 6, 1, 1000, 999, 0];
+        println!("test sort before -> {:?}", list);
         bubble_sort::sort(&mut list[..]);
-        //        list[0] = 2345;
-        println!("{:?}", list);
+        println!("test sort after -> {:?}", list);
     }
 
     #[test]
     fn test_quick_sort() {
         let mut list = [6, 1, 2, 7, 9, 3, 4, 5, 10, 8];
+        println!("test sort before -> {:?}", list);
+        //        let mut list = [6, 1];
         quick_sort::sort(&mut list[..]);
-        println!("{:?}", list);
+        println!("test sort after -> {:?}", list);
     }
 }
--- sort/src/quick_sort.rs
+++ sort/src/quick_sort.rs
@@ -1,4 +1,42 @@
+use crate::swap;
+use std::cmp::PartialOrd;
 
-pub fn sort(list: &mut [i32]) {
+pub fn sort<T>(list: &mut [T])
+where
+    T: PartialOrd + Copy,
+{
+    let len = list.len();
+    if len <= 1 {
+        return;
+    }
 
+    let value = list[0];
+    let mut l_index = 0;
+    let mut r_index = len;
+
+    let mut is_right = true;
+
+    while l_index < r_index {
+        if is_right {
+            r_index -= 1;
+
+            if list[r_index] < value {
+                is_right = false;
+            }
+        } else {
+            l_index += 1;
+
+            if list[l_index] > value {
+                swap(list, l_index, r_index);
+                is_right = true;
+            }
+        }
+    }
+
+    swap(list, 0, l_index);
+
+    if l_index > 1 {
+        sort(&mut list[..l_index]);
+        sort(&mut list[l_index + 1..]);
+    }
 }
